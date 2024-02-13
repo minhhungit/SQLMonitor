af54b5d8e9aa3ab9e35800957fc2ad6fb5aff7c9
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-02-13 08:41:45 +0530

    Updated BlitzIndex Queries

--- Sql-Queries/QRY-INDEXING-DEATH-Method-DeepDive.sql
+++ Sql-Queries/QRY-INDEXING-DEATH-Method-DeepDive.sql
@@ -13,9 +13,11 @@ declare @run_datetime_mode4 datetime = '2024-01-26 20:40:00.000';
 set @params = '@more_info_filter varchar(2000), @run_datetime_mode0 datetime, @run_datetime_mode4 datetime, @run_datetime_mode2 datetime, @table_name varchar(2000) output';
 
 set quoted_identifier off;
-set @more_info_filter = "EXEC dbo.sp_BlitzIndex @DatabaseName='facebook', @SchemaName='dbo', @TableName='LikedDislikes';";
+set @more_info_filter = "EXEC dbo.sp_BlitzIndex @DatabaseName='Facebook', @SchemaName='dbo', @TableName='LikesDislikes';";
 
 set @sql = "
+if object_id('tempdb..#BlitzIndex_Mode0') is not null
+	drop table #BlitzIndex_Mode0;
 select [result] = 'High-Priority', finding, 
 		details = case when finding = 'Indexaphobia: High Value Missing Index' 
 						then RIGHT(details, LEN(details)-CHARINDEX(' Est. benefit per day:',details))
@@ -26,11 +28,50 @@ select [result] = 'High-Priority', finding,
 								else index_size_summary
 								end, 
 		index_usage_summary, more_info, create_tsql, sample_query_plan, total_forwarded_fetch_count, url
+into #BlitzIndex_Mode0
 from dbo.BlitzIndex_Mode0 bi
 where 1=1
 --and bi.priority = -1 -- Use it to find out stats for max UpTime Days
 and bi.run_datetime = @run_datetime_mode0
 "+(case when @more_info_filter is null then '-- ' else '' end)+"and bi.more_info = @more_info_filter;
+
+select * from #BlitzIndex_Mode0;
+
+print '/* =================== HIGH VALUE MISSING INDEX: BEGIN ================================== */';
+declare @c_details nvarchar(2000);
+declare @c_index_size_summary nvarchar(max);
+declare @_equality_columns nvarchar(2000);
+declare @_inequality_columns nvarchar(2000);
+declare @_include_columns nvarchar(max);
+
+declare cur_index cursor local fast_forward for
+	select details, index_size_summary from #BlitzIndex_Mode0 where finding = 'Indexaphobia: High Value Missing Index';
+
+open cur_index;
+fetch next from cur_index into @c_details, @c_index_size_summary;
+
+while @@fetch_status = 0
+begin
+	print '-- '+@c_details
+	if(@c_index_size_summary LIKE 'EQUALITY:%')
+	begin
+		set @c_index_size_summary = REPLACE(@c_index_size_summary,'INEQUALITY:',char(13)+char(9)+'INEQUALITY:');
+		set @c_index_size_summary = REPLACE(@c_index_size_summary,'INCLUDE:',char(13)+char(9)+'INCLUDE:')
+	end
+	else
+	begin
+		set @c_index_size_summary = REPLACE(@c_index_size_summary,'INCLUDE:',char(13)+char(9)+'INCLUDE:')
+	end	
+	print @c_index_size_summary
+	print ''
+
+	fetch next from cur_index into @c_details, @c_index_size_summary;
+end
+
+CLOSE cur_index;  
+DEALLOCATE cur_index;
+
+print '/* =================== HIGH VALUE MISSING INDEX: END ================================== */';
 
 select [result] = 'Index-Usage', [table_name] = quotename(bi.database_name)+'.'+quotename(bi.schema_name)+'.'+quotename(bi.table_name),
 		bi.index_name, bi.index_definition, bi.index_size_summary, bi.data_compression_desc, 
@@ -59,7 +100,7 @@ begin
 end
 ";
 set quoted_identifier on;
-print @sql;
+--print @sql;
 exec sp_executesql @sql, @params, @more_info_filter, @run_datetime_mode0, @run_datetime_mode4, @run_datetime_mode2, @table_name = @table_name output;
 
 if (@table_name is not null)
@@ -93,7 +134,7 @@ begin
 	FROM StatsOnTable sts ORDER BY [stats_columns];
 	"
 	set quoted_identifier on;
-	print @sql
+	--print @sql
 	exec sp_ExecuteSql @sql, N'@table_name varchar(255), @database_name varchar(255)', @table_name, @database_name;
 end
 
