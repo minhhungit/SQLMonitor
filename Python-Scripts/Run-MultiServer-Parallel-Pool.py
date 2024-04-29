794a6b356df49790821ac6cbcbc98a38406475fc
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-04-29 10:18:50 +0530

    #38 updated multi server query

--- Python-Scripts/Run-MultiServer-Parallel-Pool.py
+++ Python-Scripts/Run-MultiServer-Parallel-Pool.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(description="Script to execute sql query on mul
                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-s", "--inventory_server", type=str, required=False, action="store", default="localhost", help="Inventory Server")
 parser.add_argument("-d", "--inventory_database", type=str, required=False, action="store", default="DBA", help="Inventory Database")
+parser.add_argument("--app_name", type=str, required=False, action="store", default="(dba) Run-MultiServerQuery", help="Application Name")
 
 args=parser.parse_args()
 
@@ -19,11 +20,13 @@ today = datetime.today()
 today_str = today.strftime('%Y-%m-%d')
 inventory_server = args.inventory_server
 inventory_database = args.inventory_database
+app_name = args.app_name
 
 # Get list of servers from Inventory
 invCon = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                       f"Server={inventory_server};"
                       f"Database={inventory_database};"
+                      f"App={app_name};"
                       "Trusted_Connection=yes;")
 
 invCursor = invCon.cursor()
@@ -42,17 +45,25 @@ invCursor.close()
 invCon.close()
 
 def query_server(server_row):
+    #app_name = "(dba) Run-MultiServerQuery"
+    #app_config = {"app_name": app_name}
     server = server_row.sql_instance
     port = server_row.sql_instance_port
     if port is None:
       port = 1433
     database = server_row.database
     print(f"Working on [{server}].[{database}]..")
-
+    
+    connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server},{port};DATABASE={database};Trusted_Connection=yes;TrustServerCertificate=YES;App={app_name};'
+    '''
     cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                       f"Server={server},{port};"
                       f"Database={database};"
+                      f"App={app_name};"
                       "Trusted_Connection=yes;")
+    '''
+
+    cnxn = pyodbc.connect(connectionString)
     
     sql_query = f"""
     set nocount on;
